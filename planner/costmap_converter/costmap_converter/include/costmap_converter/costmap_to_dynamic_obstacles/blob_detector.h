a1b2f6db43132461707af90f636fca2c9b39e9d9
klausmikealsontheking <hssathyakailash.ec18@gmail.com>
2025-04-12 03:01:56 +0530

    undo costmap_converter changes

--- planner/costmap_converter/costmap_converter/include/costmap_converter/costmap_to_dynamic_obstacles/blob_detector.h
+++ planner/costmap_converter/costmap_converter/include/costmap_converter/costmap_to_dynamic_obstacles/blob_detector.h
@@ -90,8 +90,7 @@ public:
   const std::vector<std::vector<cv::Point>>& getContours() { return contours_; }
 
   //! Update internal parameters
-  virtual void setParams(const cv::SimpleBlobDetector::Params& parameters) override;
-  virtual cv::SimpleBlobDetector::Params getParams() const override;
+  void updateParameters(const cv::SimpleBlobDetector::Params& parameters);
 protected:
   struct Center
   {
